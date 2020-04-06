3444b71ec7de3652d73c2917db44fcecd86a8e09
Perlerin Vincent <vperlerin@gmail.com>
2020-04-06 16:48:03 +0200

    Up

--- src/js/main.js
+++ src/js/main.js
@@ -3,11 +3,5 @@
    actionsOnSVGState();
    actionOnTable();
 
-   $("table thead th:eq(0)").data("sorter", false);
-   $('table').tablesorter({
-      widgets        : ['zebra', 'columns'],
-      usNumberFormat : false,
-      sortReset      : true,
-      sortRestart    : true
-   });
+   launch_sorter();
 })
