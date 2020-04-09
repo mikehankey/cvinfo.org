04a73f1909aa20a245b91a7d40a4d2a5eaf80a0a
mikehankey <mike.hankey@gmail.com>
2020-04-09 07:01:33 -0400

    upda

--- covid.py
+++ covid.py
@@ -70,7 +70,7 @@ else:
 
 # UPDATE THIS NUMBER WHEN THE JS or CSS ARE CACHED
 # AND RE-RENERATE THE TEMPLATE
-CUR_VERSION = '1.28.4'
+CUR_VERSION = '1.28.5'
 
 
 # Used for the dropdow above the animated maps on the state page
