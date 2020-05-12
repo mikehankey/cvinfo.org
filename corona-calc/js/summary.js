8f14fe51d8b12dfb6249a74d3d08b42f7a1005d8
Vincent Perlerin <vperlerin@gmail.com>
2020-05-12 13:43:17 +0200

    Update summary.js

--- corona-calc/js/summary.js
+++ corona-calc/js/summary.js
@@ -13,88 +13,64 @@ function fillPredictedOutcome(data,cur,trend7,trend14) {
 
    tbody =  fillPredictedOutComeRow("Deaths",data.pop, cur.deaths, trend14.deaths, trend7.deaths);
    tbody += fillPredictedOutComeRow("Confirmed Cases",data.pop, cur.conf_case,  trend14.conf_case, trend7.conf_case);
-   tbody += fillPredictedOutComeRow("Non-Tracked Infected",data.pop, cur.total_infected, trend14.total_infected, trend7.total_infected);
+   tbody += fillPredictedOutComeRow("Non-Tracked Infected",data.pop, cur.total_non_tracked_infected, trend14.total_non_tracked_infected, trend7.total_non_tracked_infected);
    tbody += fillPredictedOutComeRow("Not Infected",data.pop, cur.not_infected,  trend14.not_infected, trend7.not_infected); 
    $('#new_trends tbody').html(tbody);
 } 
 
 // Fill Top Table & Pie Chars
-function fill_top_table(data,phantom, herd_immunity_info) {
-   var tbody, total = data.pop; 
- 
-   var total_non_tracked_infected = (data.total_case/phantom)-data.total_death;
-   var total_not_infected = data.pop-total_non_tracked_infected-data.total_case;
+function fill_top_table(data,phantom) {
    
    var herd_7 = data.trend_7.reach==-1?true:false; // It means herd immunity
    var herd_14 = data.trend_14.reach==-1?true:false; 
+ 
    
-   var cur_table_data =  {
-      deaths: 0,
-      conf_case: 0,
-      non_tracked_case: 0,
-      not_infected: 0,
-      total_infected: 0
-   };
-
-
-   var trend7_table_data = {
-         deaths: 0,
-         conf_case: 0,
-         non_tracked_case: 0,
-         not_infected: 0,
-         total_infected: 0
-   };
-  
-   var trend14_table_data =  {
-      deaths: 0,
-      conf_case: 0,
-      non_tracked_case: 0,
-      not_infected: 0,
-      total_infected: 0
-   };
+   var cur_table_data =  {  deaths: 0, conf_case: 0,  non_tracked_case: 0, not_infected: 0,  total_infected: 0  };
+   var trend7_table_data =  {  deaths: 0, conf_case: 0,  non_tracked_case: 0, not_infected: 0,  total_infected: 0  };
+   var trend14_table_data =  {  deaths: 0, conf_case: 0,  non_tracked_case: 0, not_infected: 0,  total_infected: 0  };
 
    // Herd or 0
    if(!herd_7) {
       $('#7_day_date').text(data.trend_7.reach);
- 
-      // We prepare the data
-      trend7_table_data.conf_case               = parseInt(data.trend_7.total_at_end);
-      trend7_table_data.deaths                  = parseInt(data.trend_7.total_at_end*data.last_mortality_rate/100);
-      trend7_table_data.total_infected          = parseInt((trend7_table_data.conf_case / phantom ) + trend7_table_data.conf_case);
-      trend7_table_data.not_infected            = data.pop -  trend7_table_data.total_infected -  trend7_table_data.deaths;
- 
- 
-
    } else {
-      $('#7_day_date').text(dateFormatMITFromDate(herd_immunity_info.trend_7.herd_immunity_date));
+     $('#7_day_date').text(dateFormatMITFromDate(data.trend_7.herd_reach_date));
    }
+   
+   // We prepare the data
+   trend7_table_data.conf_case               = parseInt(data.trend_7.total_at_end);
+   trend7_table_data.deaths                  = parseInt(data.trend_7.total_at_end*data.last_mortality_rate/100);
+   trend7_table_data.total_non_tracked_infected          = parseInt( trend7_table_data.conf_case / phantom ) ;
+   trend7_table_data.not_infected            = data.pop -  trend7_table_data.total_non_tracked_infected -  trend7_table_data.deaths;
    
    // Herd or 0
    if(!herd_14) {
       $('#14_day_date').text(data.trend_14.reach);
-
-       // We prepare the data
-       trend14_table_data.conf_case               = parseInt(data.trend_14.total_at_end);
-       trend14_table_data.deaths                  = parseInt(data.trend_14.total_at_end*data.last_mortality_rate/100);
-       trend14_table_data.total_infected          = parseInt((trend14_table_data.conf_case / phantom ) + trend14_table_data.conf_case);
-       trend14_table_data.not_infected            = data.pop -  trend7_table_data.total_infected -  trend14_table_data.deaths;
-  
-
    } else {
-      $('#14_day_date').text(dateFormatMITFromDate(herd_immunity_info.trend_14.herd_immunity_date));
-   }
+      $('#14_day_date').text(dateFormatMITFromDate(data.trend_14.herd_reach_date));
+   } 
    
-   
+   // We prepare the data
+   trend14_table_data.conf_case               = parseInt(data.trend_14.total_at_end);
+   trend14_table_data.deaths                  = parseInt(data.trend_14.total_at_end*data.last_mortality_rate/100);
+   trend14_table_data.total_non_tracked_infected          = parseInt( trend14_table_data.conf_case / phantom ) ;
+   trend14_table_data.not_infected            = data.pop -  trend14_table_data.total_non_tracked_infected -  trend14_table_data.deaths;
+ 
+ 
    // Cur Data 
    cur_table_data.conf_case               = parseInt(data.total_case);
    cur_table_data.deaths                  = parseInt(data.total_death);
-   cur_table_data.total_infected          = parseInt((data.total_case / phantom ) + data.total_case);
-   cur_table_data.not_infected            = data.pop -   cur_table_data.total_infected  -  cur_table_data.deaths;
+   cur_table_data.total_non_tracked_infected          = parseInt( data.total_case / phantom )  ;
+   cur_table_data.not_infected            = data.pop -   cur_table_data.total_non_tracked_infected  -  cur_table_data.deaths;
 
-   console.log("FILL PRED");
-   fillPredictedOutcome(data,cur_table_data, trend7_table_data,trend14_table_data );
  
-
+   fillPredictedOutcome(data,cur_table_data, trend7_table_data,trend14_table_data );
+  
+   // Build the pies
+   // Create Pies Pies
+   var pie_lb = [ 'Not Infected', 'Infected', 'Confirmed Cases', 'Deaths'];
+   plot_pie([trend14_table_data.not_infected, trend14_table_data.total_non_tracked_infected, trend14_table_data.conf_case,  trend14_table_data.deaths],pie_lb,"14-Day Trend","new_cases_pie_14");
+   plot_pie([trend7_table_data.not_infected, trend7_table_data.total_non_tracked_infected, trend7_table_data.conf_case,  trend7_table_data.deaths],pie_lb,"7-Day Trend","new_cases_pie_7");
+ 
 }
  
 
@@ -128,34 +104,35 @@ function create_top_page(data, phantom) {
          }
       }
 
-      // Fill Top Table
-      fill_top_table(data, phantom);
+    
 
    } else {
-     
+
+ 
       if(data.trend_7['reach']==-1 && data.trend_14['reach']!=-1  ) { 
          
          // We need to compute the Herd Immunity for 7 day trend only
-         herd_immunity_info  = get_herd_immunity_info(data,7, phantom);
          top_sentence = "Based on the 7-day trend,<br>" +  data.name;
-         top_sentence += " could reach <span class='ugly_t'>herd immunity on <span class='wn'>" + dateFormatMITFromDate(herd_immunity_info.trend_7.herd_immunity_date) + "</span></span>";
+         top_sentence += " could reach <span class='ugly_t'>herd immunity on <span class='wn'>" + dateFormatMITFromDate(data.trend_7['herd_reach_date']) + "</span></span>";
          top_sentence += "<br>but based on the 14-day trend,<br>";
          top_sentence += " it could have <span class='good_t'>zero cases by <span class='wn'>" + data.trend_14['reach']  + "</span>.";
 
       } else if(data.trend_7['reach']!=-1 && data.trend_14['reach']==-1) {
 
-         // We need to compute the Herd Immunity for 14 day trend only
-         herd_immunity_info  = get_herd_immunity_info(data,14, phantom);
+        
+         // We need to compute the Herd Immunity for 14 day trend only 
          top_sentence = "Based on the 14-day trend,<br>" +  data.name;
-         top_sentence += " could reach <span class='ugly_t'>herd immunity on <span class='wn'>" + dateFormatMITFromDate(herd_immunity_info.trend_14.herd_immunity_date) + "</span></span>";
+         top_sentence += " could reach <span class='ugly_t'>herd immunity on <span class='wn'>" + dateFormatMITFromDate(data.trend_14['herd_reach_date']) + "</span></span>";
          top_sentence += "<br>but based on the 7-day trend,<br>";
          top_sentence += " it could have <span class='good_t'>zero cases by <span class='wn'>" + data.trend_7['reach']  + "</span>.";
+       
 
       } else { 
 
-         // We need to compute the Herd Immunity for both trends
-         herd_immunity_info  = get_herd_immunity_info(data,-1, phantom);
- 
+         //if(data.trend2['herd_reach_day'] < ) {
+
+         
+         /*
          
          if(herd_immunity_info.trend_7.herd_immunity_date == herd_immunity_info.trend_14.herd_immunity_date) {
             top_sentence = "Based on the latest trends,<br>" +  data.name;
@@ -174,16 +151,14 @@ function create_top_page(data, phantom) {
             top_sentence += " <span class='wn'>" + dateFormatMITFromDate(herd_immunity_info.trend_7.herd_immunity_date) + "</span>";
             top_sentence += "</span>.";
           }
+          */
  
 
-      }
-
-       // Fill Top Table
-       fill_top_table(data, phantom, herd_immunity_info);
-       
-
+      } 
    }
 
+   // Fill Top Table & Pies
+   fill_top_table(data, phantom);
 
    // Create top Sentence
    $('#top_summary').html("<div id='sum_main'>"+top_sentence+"</div>"); 
@@ -192,10 +167,5 @@ function create_top_page(data, phantom) {
 
 function createSummary(data) {
   var phantom = 1/4;
-
-
-   console.log("createSummary ");
-   console.log(data);
-
   create_top_page(data, phantom);
 }
