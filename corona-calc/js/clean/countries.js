67b8562b41a2b155092f61c631e745ff6bc6d70c
Perlerin Vincent <vperlerin@gmail.com>
2020-05-20 11:53:39 +0200

    International

--- corona-calc/js/clean/countries.js
+++ corona-calc/js/clean/countries.js
@@ -1,32 +1,45 @@
+function compare( a, b ) {
+   if ( a.date < b.date ){
+     return -1;
+   }
+   if ( a.date > b.date ){
+     return 1;
+   }
+   return 0;
+ }
+ 
+ 
+
 
 /**
  * Return the proper data based on Country
  */
-function getInitDataCountry(data,country, country_name) {
-   var name, toReturn;
-  
-
+function getInitDataCountry(data,country) {
+   var toReturn;
+   
    var country_stats = [], all_dates = [];
 
-   $.each(data,function(i,v) {
+   $.each(data,function(i,v) { 
       country_stats.push({
-         'confirmed': v.Confirmed,
-         'deaths': v.Deaths,
-         'recovered': v.Recovered,
-         'active': v.Active,
-         'date': v.Date
+         'date':                    i,
+         'new_cases_per_million':   parseFloat(v.ncpm)>=0?parseFloat(v.ncpm):0,
+         'new_deaths_per_million':  parseFloat(v.ndpm)>=0?parseFloat(v.ndpm):0,
+         'total_cases_per_million': parseFloat(v.tcpm)>=0?parseFloat(v.tcpm):0,
+         'total_deaths_per_million':parseFloat(v.tdpm)>=0?parseFloat(v.tdpm):0 
       });
-      all_dates.push(v.Date);
+      all_dates.push(new Date(i));
    })
-
  
+   // Sort country states per date
+   country_stats.sort( compare );
+
    // State Data
    toReturn =  { 
-      name:       country_name,
-      slug:       country,
+      name:       country, 
       stats:      country_stats,
       last_date:  new Date(Math.max.apply(null,all_dates))
    };  
+ 
   
    return toReturn;
 }
@@ -36,33 +49,38 @@ function getInitDataCountry(data,country, country_name) {
  */
 function prepareData(data) {
 
-   // Confirmed
-   var x_axis_confirmed  = [];   // They all share the same X axis
-   var y_axis_confirmed = [];
-   // New Deaths 
-   var y_axis_deaths = [];
-   // New Recovered 
-   var y_axis_recovered = [];
-   // New Active 
-   var y_axis_active = [];
+   
+   var x_axis  = [];  
+
+   // new_cases_per_million  
+   var y_axis_ncpm = [];
+
+   // new_deaths_per_million 
+   var y_axis_ndpm = [];
+   
+   // total_cases_per_million 
+   var y_axis_tcpm = [];
+
+   // total_deaths_per_million 
+   var y_axis_tdpm = [];
+ 
 
    $.each(data['stats'], function(i,val) { 
-  
-      x_axis_confirmed.push(val.date);
-
-      y_axis_confirmed.push(val.confirmed);
-      y_axis_deaths.push(val.deaths);
-      y_axis_recovered.push(val.recovered);
-      y_axis_active.push(val.active);
+      x_axis.push(val.date);
+      y_axis_ncpm.push(val.new_cases_per_million);
+      y_axis_ndpm.push(val.new_deaths_per_million);
+      y_axis_tcpm.push(val.total_cases_per_million);
+      y_axis_tdpm.push(val.total_deaths_per_million);
+       
       
    });
     
    return {
-      x_axis:     x_axis_confirmed,
-      confirmed:  y_axis_confirmed,
-      deaths:     y_axis_deaths,
-      recovered:  y_axis_recovered,
-      active:     y_axis_active
+      x_axis:     x_axis,
+      new_cases_per_million:        y_axis_ncpm,
+      new_deaths_per_million:       y_axis_ndpm,
+      total_cases_per_million:      y_axis_tcpm,
+      total_deaths_per_million:     y_axis_tdpm
    }
 }
 
@@ -76,31 +94,31 @@ function prepareData(data) {
  */
 function draw_country_graph(init_data,graph_data) {
    
- 
-
+  
    // Org Data
-   var dataDeaths = {
+   var dateNCPM = {
       x: graph_data.x_axis,
-      y: graph_data.deaths,
-      name: "Deaths",
+      y: graph_data.new_deaths_per_million,
+      name: "New Deaths Per Million",
       type: "bar",
-      yaxis: 'y2',
+      //yaxis: 'y2',
       marker: {color: 'rgba(55, 83, 109,.2)'},
    };
  
-   var dataConfirmed = {
+   var dataNDPM = {
       x: graph_data.x_axis,
-      y: graph_data.confirmed,
-      name: "Confirmed Cases",
+      y: graph_data.new_cases_per_million,
+      name: "New Cases Per Million",
       type: 'scatter',
       mode: 'lines',
-      yaxis: 'y1',
+      //yaxis: 'y1',
       line: {
          color: 'rgb(55, 128, 191)',
          width: 3
        }
    };
 
+   /*
    var dataRecovered = {
       x: graph_data.x_axis,
       y: graph_data.recovered,
@@ -118,14 +136,14 @@ function draw_country_graph(init_data,graph_data) {
       mode: 'lines',
       yaxis: 'y1',
    };
-   
+   */
 
    var layout = { 
       margin: {"t": 20, "b": 80, "l": 80, "r": 80},
       showlegend: true,
       legend: { orientation: "h" },
       yaxis2: { 
-         title: 'Total Deaths', 
+         title: 'Deaths  per million', 
          overlaying: 'y',
          side: 'right',
          rangemode: 'nonnegative',
@@ -138,7 +156,7 @@ function draw_country_graph(init_data,graph_data) {
          showticklabels: false
        },
        yaxis1: {  
-         title: 'Total Cases', 
+         title: 'Cases per million', 
          side: 'left',
          rangemode: 'nonnegative'
        }
@@ -148,7 +166,7 @@ function draw_country_graph(init_data,graph_data) {
 
  
  
-   all_set =  [ dataConfirmed, dataRecovered, dataActive, dataDeaths];
+   all_set =  [ dateNCPM, dataNDPM];
    Plotly.newPlot('country_graph', all_set, layout);
 
    $('#country_graph_title').html('<h3>' + init_data.name +  '</h3>');
