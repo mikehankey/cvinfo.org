dfd880165542bf4882299ff05365ec77b295e32a
Perlerin Vincent <vperlerin@gmail.com>
2020-06-23 14:37:21 +0200

    Up

--- python/create_main_gbu_page.py
+++ python/create_main_gbu_page.py
@@ -213,11 +213,12 @@ def generate_gbu_graphs_and_main_page(groups):
 # Create State HTML Element with image
 def create_state_DOM_el(st,all_state_details,rand) :
    return '<div class="graph_g">\
-            <h3 class="nmb">'+US_STATES[st]+'<small>&Delta;14: '+ str(all_state_details['delta14']) +' -  &Delta;7: '+ str(all_state_details['delta7']) +'</small></h3>\
+            <h3 class="nmb">'+US_STATES[st]+'</h3>\
+            <h4>&Delta;14: '+ str(all_state_details['delta14']) +' -  &Delta;7: '+ str(all_state_details['delta7']) +'</small><br>\
+            <small>New cases on '+all_state_details['last_update']+': ' +  display_us_format(all_state_details['last_new_cases'],0)  + '</small>\
             <a href="./'+st+'/index.html"><img src=".'+os.sep+st+os.sep+st+'.png?v='+rand+'" width="345" alt="'+US_STATES[st]+'"/></a>\
             <small>Total Cases: '+display_us_format(all_state_details['total_case'],0) +' - Total Deaths: '+ display_us_format(all_state_details['total_death'],0) +'</small></div>' 
  
-
 if __name__ == "__main__":
    os.system("clear")
    generate_gbu_graphs_and_main_page(rank_states())
