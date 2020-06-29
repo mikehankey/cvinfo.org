3369a0a26379e5c4e79e8dcd83b245dfa4661e49
Perlerin Vincent <vperlerin@gmail.com>
2020-06-29 20:46:08 +0200

    Social Share Fix

--- corona-calc/js/clean/share.js
+++ corona-calc/js/clean/share.js
@@ -22,53 +22,56 @@ function setShareLinks(data) {
    var title = $("meta[property='og:description']").attr("content");
    var initUrl =  $.trim($("meta[property='og:url']").attr("content"));
   
-
-   // Update the UWASH Link
-   uwash_state_link = 'https://covid19.healthdata.org/united-states-of-america/'+ data['state'].replace(' ','-').toLowerCase();
-   $('#UWASH_link').attr('href',uwash_state_link).text(uwash_state_link);
-   $('#uwash').css('display','inline').find('span').html(data['state']);
+   if( data !== undefined  ) {
 
 
-   if(typeof data == "undefined") {
-       pageUrl = document.URL;
-   } else {
-      if(typeof data.state !== "undefined" && typeof data.state_code !== "undefined" ) {
-         title += " for " + data.state; 
+      // Update the UWASH Link
+      uwash_state_link = 'https://covid19.healthdata.org/united-states-of-america/'+ data.state.replace(' ','-').toLowerCase();
+      $('#UWASH_link').attr('href',uwash_state_link).text(uwash_state_link);
+      $('#uwash').css('display','inline').find('span').html(data['state']);
 
-         
-         if(typeof data.county !== "undefined" && data.county!="") {
-            // If we have a county
-            pageUrl = initUrl + "?" + data.state_code + "+" + data.county;
-         } else {
-            // If we have a just a state 
-            pageUrl = initUrl + "?" + data.state_code;
+
+      if(typeof data == "undefined") {
+         pageUrl = document.URL;
+      } else {
+         if(typeof data.state !== "undefined" && typeof data.state_code !== "undefined" ) {
+            title += " for " + data.state; 
+
+            
+            if(typeof data.county !== "undefined" && data.county!="") {
+               // If we have a county
+               pageUrl = initUrl + "?" + data.state_code + "+" + data.county;
+            } else {
+               // If we have a just a state 
+               pageUrl = initUrl + "?" + data.state_code;
+            }
+
          }
-
       }
+   
+      title = encodeURIComponent(title);
+      pageUrl = encodeURIComponent(pageUrl);
+      
+
+      $(".social-share.facebook").unbind("click").on("click", function() {
+         url = "https://www.facebook.com/sharer.php?u=" + pageUrl;
+         socialWindow(url);
+      });
+
+      $(".social-share.twitter").unbind("click").on("click", function() {
+         url = "https://twitter.com/intent/tweet?url=" + pageUrl + "&text=" + title;
+         socialWindow(url);
+      });
+
+      $(".social-share.linkedin").unbind("click").on("click", function() {
+         url = "https://www.linkedin.com/shareArticle?mini=true&url=" +  decodeURIComponent(pageUrl);
+         socialWindow(url);
+      });
+
+      $(".social-share.reddit").unbind("click").on("click", function() { 
+         url = "http://www.reddit.com/submit?url=" + pageUrl + "&title=" +  title;
+         socialWindow(url);
+   })
    }
- 
-   title = encodeURIComponent(title);
-   pageUrl = encodeURIComponent(pageUrl);
-     
-
-   $(".social-share.facebook").unbind("click").on("click", function() {
-       url = "https://www.facebook.com/sharer.php?u=" + pageUrl;
-       socialWindow(url);
-   });
-
-   $(".social-share.twitter").unbind("click").on("click", function() {
-       url = "https://twitter.com/intent/tweet?url=" + pageUrl + "&text=" + title;
-       socialWindow(url);
-   });
-
-   $(".social-share.linkedin").unbind("click").on("click", function() {
-       url = "https://www.linkedin.com/shareArticle?mini=true&url=" +  decodeURIComponent(pageUrl);
-       socialWindow(url);
-   });
-
-   $(".social-share.reddit").unbind("click").on("click", function() { 
-      url = "http://www.reddit.com/submit?url=" + pageUrl + "&title=" +  title;
-      socialWindow(url);
-  })
    
 }  
